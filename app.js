a24a041fb9f99de357180f472c90b4ea4ae168bc
ajikowiyu <ajikowiyu@gmail.com>
2026-02-13 20:07:44 +0700

    basic routing

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hello world')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h1>Halaman Utama</h1><hr>
+        <p>lorem ipsum lorem ipsum</p><br>
+        <button>simpan</button>`
+    )
+})
+
+app.get('/about', (req, res) => {
+    res.send(
+        `<h1>Halaman Profil Kami</h1><hr>
+        <p>PT Bangun Abadi adalah perusahaan yg bergerak di bidang IT</p><br>
+        <button>simpan</button>`
+    )
+})
+
+
+app.listen(port, () => {
+    console.log(`Aplikasi berjalan di http://localhost:${port}`)
+})
